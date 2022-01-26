fe23058eddf15f81122a206aae5cc19175898433
JosiasAurel <ndjosiasaurel@gmail.com>
2022-01-26 02:29:56 +0100

    add router

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,13 @@ import "./styles/index.css";
 import HomePage from "./Home";
 import { NextUIProvider } from "@nextui-org/react";
 import { ThirdwebProvider } from "@3rdweb/react";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 const supportedChains: Array<number> = [80001];
 
 const connectors = {
   injected: {},
 };
+
 
 ReactDOM.render(
   <NextUIProvider>
@@ -18,7 +19,12 @@ ReactDOM.render(
       supportedChainIds={supportedChains}
     >
       <React.StrictMode>
-        <HomePage />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route index element={<HomePage />} />
+          </Routes>
+        </BrowserRouter>
       </React.StrictMode>
     </ThirdwebProvider>
   </NextUIProvider>,
